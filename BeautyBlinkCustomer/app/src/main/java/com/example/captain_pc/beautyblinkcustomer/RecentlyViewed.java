93d52baa1633a5d2c0bc32a6bb8ad9d1ffbbff65
Unknown <chanasit.captain@hotmail.com>
2017-01-20 16:46:26 +0700

    Customer's Setting

---
+++ BeautyBlinkCustomer/app/src/main/java/com/example/captain_pc/beautyblinkcustomer/RecentlyViewed.java
@@ -0,0 +1,10 @@
+package com.example.captain_pc.beautyblinkcustomer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CaptainPC on 20/1/2560.
+ */
+
+public class RecentlyViewed extends AppCompatActivity {
+}
