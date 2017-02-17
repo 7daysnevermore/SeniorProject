e06b8898e1315295b121430a482cc8f4bbbe00f1
Unknown <sosotidaporn@gmail.com>
2017-02-17 19:06:20 +0800

    edit something a arai wa

--- BeautyBlinkCustomer/app/src/main/java/com/example/captain_pc/beautyblinkcustomer/SearchDetails.java
+++ BeautyBlinkCustomer/app/src/main/java/com/example/captain_pc/beautyblinkcustomer/SearchDetails.java
@@ -2,13 +2,20 @@ package com.example.captain_pc.beautyblinkcustomer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -27,6 +34,15 @@ import com.example.captain_pc.beautyblinkcustomer.fragments.SearchPrice;
 import com.example.captain_pc.beautyblinkcustomer.model.DataProfilePromote;
 import com.example.captain_pc.beautyblinkcustomer.model.SearchViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -40,7 +56,7 @@ import java.util.HashMap;
  * Created by NunePC on 29/1/2560.
  */
 
-public class SearchDetails extends AppCompatActivity implements View.OnClickListener {
+public class SearchDetails extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
 
     private RecyclerView recyclerView;
@@ -55,6 +71,12 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
     public ImageView icon_search,icon_filter,up,down,tab_popular,tab_nearby,tab_latest,tab_price;
     public boolean checking;
     int clickcount=0;
+
+    private static final String LOG_TAG = "PlacesAPIActivity";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    public Double cur_lat,cur_lng;
 
 
     @Override
@@ -84,6 +106,14 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
         wording = getIntent().getStringExtra("word");
         lat = getIntent().getStringExtra("lat");
         lng = getIntent().getStringExtra("lng");
+
+
+        // Build the Play services client for use by the Fused Location Provider and the Places API.
+        mGoogleApiClient = new GoogleApiClient.Builder(SearchDetails.this)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .build();
+        mGoogleApiClient.connect();
 
         word.setHint(search);
         word.setFocusable(true);
@@ -120,6 +150,19 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.price).setOnClickListener(this);
         findViewById(R.id.icon_filter).setOnClickListener(this);
 
+        if (mGoogleApiClient.isConnected()) {
+            if (ContextCompat.checkSelfPermission(SearchDetails.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(SearchDetails.this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQUEST_CODE);
+            } else {
+                callPlaceDetectionApi();
+            }
+
+        }
+
     }
 
     @Override
@@ -127,27 +170,6 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
         super.onStart();
 
 
-    }
-
-    /** calculates the distance between two locations in MILES */
-    private double distance(double lat1, double lng1, double lat2, double lng2) {
-
-        double earthRadius = 3958.75; // in miles, change to 6371 for kilometer output
-
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-
-        double sindLat = Math.sin(dLat / 2);
-        double sindLng = Math.sin(dLng / 2);
-
-        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
-                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-        double dist = earthRadius * c;
-
-        return dist; // output distance, in MILES
     }
 
     @Override
@@ -205,6 +227,7 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
                         .commit();
                 break;
             case R.id.nearby:
+
                 if(previous.equals("popular")){
                     tab_popular.setVisibility(View.GONE);
                     popular.setTextColor(getResources().getColor(R.color.streak_color_light));
@@ -271,6 +294,58 @@ public class SearchDetails extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(this,
+                "Google Places API connection failed with error code:" +
+                        connectionResult.getErrorCode(),
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPlaceDetectionApi();
+                }
+                break;
+        }
+    }
+
+    private void callPlaceDetectionApi() throws SecurityException {
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i(LOG_TAG, String.format("Place '%s' with " +
+                                    "likelihood: %g",
+                            placeLikelihood.getPlace().getLatLng().latitude,placeLikelihood.getPlace().getLatLng().longitude,
+                            placeLikelihood.getLikelihood()));
+                    cur_lat = placeLikelihood.getPlace().getLatLng().latitude;
+                    cur_lng = placeLikelihood.getPlace().getLatLng().longitude;
+                }
+
+                likelyPlaces.release();
+            }
+        });
+    }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }
